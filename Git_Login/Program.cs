73c963c9805b7cb30b730d4e68a189570998ebbc
dedan <dedan@DESKTOP-EFSTSPH>
2025-03-19 18:14:32 +1300

    Add project files.

---
+++ Git_Login/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Multiple2or3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Enter A number between 1 and 50");
+
+        }
+    }
+}
