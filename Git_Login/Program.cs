0737709bad157cb4e54a650bba7125c59874c753
DedanV <dedanvosloo@gmail.com>
2025-03-20 17:14:38 +1300

    Update Program.cs
    Fixed

--- Git_Login/Program.cs
+++ Git_Login/Program.cs
@@ -7,7 +7,7 @@ namespace Multiple2or3
         static void Main()
         {
             Console.WriteLine("Please Enter A number between 1 and 50");
-            int User_Number = int.Parse(Console.ReadLine(123));
+            int User_Number = int.Parse(Console.ReadLine());
         }
     }
 }
