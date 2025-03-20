a6bf2f29b70223174bc0ac5075c60c56de5b5463
Dedan Vosloo <850005456@nzse.ac.nz>
2025-03-20 17:41:01 +1300

    Full code

--- Git_Login/Program.cs
+++ Git_Login/Program.cs
@@ -6,8 +6,58 @@ namespace Multiple2or3
     {
         static void Main()
         {
-            Console.WriteLine("Please Enter A number between 1 and 50");
-            int User_Number = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter any number between 1 and 50.");
+                    int User_number = int.Parse(Console.ReadLine());
+
+                    if (User_number < 1 || User_number > 50)
+                    {
+                        Console.WriteLine("Invalid input.");
+                        continue;
+                    }
+
+
+                    if (User_number % 2 == 0 && User_number % 3 == 0)
+                    {
+                        Console.WriteLine($"Your number {User_number} is a multiple of 2 and 3");
+                    }
+                    else if (User_number % 3 == 0)
+                    {
+                        Console.WriteLine($"Your number {User_number} is a multiple of 3.");
+                    }
+                    else if (User_number % 2 == 0)
+                    {
+                        Console.WriteLine($"Your number {User_number} is a multiple of 2.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number is not a multiple of 2 or 3.");
+                    }
+
+                    Console.WriteLine("Do you wish to try again. YES:1   NO:2");
+                    int User_Answer = int.Parse(Console.ReadLine());
+
+                    if (User_Answer == 2)
+                    {
+                        Console.WriteLine("Thank you or using this program.");
+                        break;
+                    }
+
+                    else if (User_Answer != 1)
+                    {
+                        Console.WriteLine("Invalid input.");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR: Invalid input try again.");
+                }
+                
+            }
+
         }
     }
 }
